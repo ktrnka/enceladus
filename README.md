fc1e586e2994354c4c3901d63ee0ee7e0241c4e1
Keith Trnka <keith.trnka@gmail.com>
2018-04-07 16:47:05 -0700

    Hard coded version with Montel

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Enceladus Stealth
 
 Because Xtranormal doesn't exist anymore.
+
+## Stuff that might be useful
+
+    window.speechSynthesis.pause()
+    window.speechSynthesis.resume()
+    window.speechSynthesis.stop()
+    
+    
+https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance/pitch
+
--- site/enceladus.js
+++ site/enceladus.js
@@ -1,5 +1,7 @@
 // cache DOM lookups
 const DOM = {
+    patient_div: $('#patient'),
+    doctor_div: $('#doctor')
 };
 
 // simple script to read from
@@ -12,8 +14,36 @@ const script = [
     {speaker: 'patient', text: 'They\'re like a dull ache you know? Sort of always there and just like that, like usually it feels that way when I don\'t sleep well'}
 ];
 
+// mapping the speaker names to SpeechSynthesisUtterance configuration
+const speechConfigs = {
+    doctor: function (utterance) {
+        // utterance.pitch = 0.8;
+        utterance.voice = findVoice("en", "Google UK English Male");
+
+        utterance.addEventListener('start', function () {
+            $('#doctor').show();
+        });
+        utterance.addEventListener('end', function () {
+            $('#doctor').hide();
+        });
+    },
+    patient: function (utterance) {
+        utterance.pitch = 0.6;
+        utterance.voice = findVoice("en", "Google US English");
+
+        utterance.addEventListener('start', function () {
+            $('#patient').show();
+        });
+        utterance.addEventListener('end', function () {
+            $('#patient').hide();
+        });
+    }
+}
+
 // utterances must be stored in globals so that the callbacks aren't garbage collected before they're spoken
 window.utterances = [];
+
+
 
 /** SPEECH API STUFF **/
 
@@ -22,27 +52,30 @@ window.utterances = [];
 
 function makeUtterance({speaker, text}) {
     const utterance = new SpeechSynthesisUtterance(text);
-    utterance.addEventListener('start', function () {
-        console.log('started ' + text);
-    });
-    utterance.addEventListener('end', function () {
-        console.log('ended ' + text);
-    });
+
+    // speaker-based configuration (note: not very safe)
+    speechConfigs[speaker](utterance);
 
     return utterance;
 }
 
 /** MAIN READER **/
-window.utterances = $.map(script, function(obj, i) { return makeUtterance(obj); });
+function buildUtterances(messages) {
+    window.utterances = $.map(messages, function (obj, i) {
+        return makeUtterance(obj);
+    });
+}
 
 function startReading() {
     console.log("Starting to read");
+    buildUtterances(script);
     $.each(window.utterances, function(i, utterance) { speechSynthesis.speak(utterance); });
 }
 
 
 $('document').ready(function() {
-    startReading();
+    waitForVoices(() => { startReading(); });
+    // startReading();
 });
 
 /** OLD **/
@@ -50,7 +83,31 @@ function speechAvailable() {
     return 'speechSynthesis' in window;
 }
 
+function logVoices() {
+    console.log("Available voices:");
+    $.each(getVoices("en"), (i, voice) => { console.log(voice); });
+}
 function getVoices(langId) {
     /* NOTE: Voices aren't all loaded right away for some reason */
     return $.grep(speechSynthesis.getVoices(), (voice) => { return voice.lang.startsWith(langId); })
+}
+
+function findVoice(langId, name) {
+    const matches = $.grep(getVoices(langId), (voice) => { return voice.name == name; });
+
+    return matches[0];
+}
+
+function waitForVoices(func_callback) {
+    setTimeout(function() {
+        console.log("Checking for voices again");
+        if (speechAvailable() && getVoices("en").length > 0) {
+            logVoices();
+            console.log(func_callback);
+            func_callback();
+        }
+        else {
+            waitForVoices(func_callback);
+        }
+    }, 500);
 }
